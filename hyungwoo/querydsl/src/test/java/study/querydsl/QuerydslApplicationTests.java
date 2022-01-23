93d1e633819ff89263429aa47612b7e5b5073bdd
wu2ee <gulb1602@gmail.com>
2022-01-23 20:31:51 +0900

    feat : section 1. 실습 코드

---
+++ hyungwoo/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,40 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+//	@PersistenceContext // 자바 표준 (spring 아닐 때 사용)
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		// querydsl 사용할 때 쿼리와 관련된 것들은 Q-type을 사용해야 함.
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+}
