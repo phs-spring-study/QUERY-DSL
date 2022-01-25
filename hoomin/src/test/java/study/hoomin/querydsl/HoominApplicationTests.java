67ba21814242385e61f22c158cfe12f9aac227ec
lhm7877 <vkfdldh7>
2022-01-26 00:43:14 +0900

    initial commit

---
+++ hoomin/src/test/java/study/hoomin/querydsl/HoominApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hoomin.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoominApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
